80bb70e0a80e6f3637d8f42c71e1f1ecffc8c100
Pocveb W. Estiros <pocveb@tutanota.com>
2021-02-27 18:46:30 +0200

    Configuration Service (WIP)

---
+++ src/main/kotlin/world/cepi/config/Configurable.kt
@@ -0,0 +1,37 @@
+package world.cepi.config
+
+import java.util.function.BiConsumer
+
+/**
+ * Represents a value that is configured, and thus
+ * subject to change.
+ *
+ * @param V The type of the value
+ */
+interface Configurable<V> {
+
+    /**
+     * Gets the current value of this configurable.
+     * The output of this method will change when the value
+     * is updated.
+     *
+     * @return The current value of this configurable
+     */
+    fun get(): V
+
+    /**
+     * Sets a [BiConsumer] callback to be executed whenever the
+     * value of this configurable is updated.
+     *
+     * The first argument passed to the callback is the new value,
+     * the second is the old value.
+     *
+     * The update might be called, even if the new and old values
+     * are the same.
+     *
+     * @param callback The [BiConsumer] callback
+     */
+    fun onUpdate(callback: BiConsumer<V, V>)
+
+}
+
