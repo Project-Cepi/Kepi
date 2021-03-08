50ddb33cdeaae56dbf2bb1032144e39fa5e9cbc1
LeoDog896 <LeoDog896@hotmail.com>
2021-03-07 19:51:09 -0500

    Add help subcommand

---
+++ src/main/kotlin/world/cepi/subcommands/Help.kt
@@ -0,0 +1,30 @@
+package world.cepi.subcommands
+
+import net.minestom.server.chat.ChatColor.*
+import net.minestom.server.command.builder.Command
+
+class Help(vararg val messages: String, name: String = "help") : Command(name) {
+
+    companion object {
+        const val head = "╓"
+        const val tail = "╙"
+
+        const val arm = "─"
+        const val armLength = 4
+
+        const val body = "║"
+    }
+
+    init {
+        setDefaultExecutor { sender, args ->
+            sender.sendMessage("$DARK_GRAY$head${arm.repeat(armLength)}")
+
+            messages.forEach {
+                sender.sendMessage("$DARK_GRAY$body $GRAY$it")
+            }
+
+            sender.sendMessage("$DARK_GRAY$tail${arm.repeat(armLength)}")
+        }
+    }
+
+}
