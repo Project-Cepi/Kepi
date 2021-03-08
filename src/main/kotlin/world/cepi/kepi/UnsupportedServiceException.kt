fd191061b2b497cd0776b5aa38b3c40d30a15888
LeoDog896 <LeoDog896@hotmail.com>
2021-03-07 19:15:39 -0500

    Remove Service Manager
    No need for it, itll be better to have strict depdencencies

--- src/main/kotlin/world/cepi/kepi/UnsupportedServiceException.kt
+++
@@ -1,8 +0,0 @@
-package world.cepi.kepi
-
-/**
- * Thrown when a service that is not implemented is requested.
- *
- * @param message The message used as the reason for this exception
- */
-class UnsupportedServiceException(override val message: String) : RuntimeException(message)
