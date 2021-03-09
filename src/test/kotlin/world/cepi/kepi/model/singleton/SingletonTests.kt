b96ae1eb633ab31994f82fdb714109fa40466219
LeoDog896 <LeoDog896@hotmail.com>
2021-03-09 11:46:13 -0500

    Explicit call to kotlinx serializer
    Requires InternalSerializationApi

--- src/test/kotlin/world/cepi/kepi/model/singleton/SingletonTests.kt
+++ src/test/kotlin/world/cepi/kepi/model/singleton/SingletonTests.kt
@@ -1,9 +1,11 @@
 package world.cepi.kepi.model.singleton
 
+import kotlinx.serialization.InternalSerializationApi
 import org.junit.jupiter.api.Test
 
 class SingletonTests {
-
+    
+    @InternalSerializationApi
     @Test
     fun `square should parse correctly`() {
 
