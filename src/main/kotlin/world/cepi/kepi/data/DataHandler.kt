5550be82659d80c48d06497da440f11c2e835b80
LeoDog896 <LeoDog896@hotmail.com>
2021-03-10 19:05:27 -0500

    Abstract away data

--- src/main/kotlin/world/cepi/kepi/data/DataHandler.kt
+++ src/main/kotlin/world/cepi/kepi/data/DataHandler.kt
@@ -4,30 +4,8 @@ import world.cepi.kepi.data.database.DatabaseHandler
 import world.cepi.kepi.data.database.implementations.MemoryDatabase
 import world.cepi.kepi.data.model.Model
 
-object DataHandler {
+object DataHandler: IDataHandler {
 
-    /**
-     * Represents the [DatabaseHandler] this [DataHandler] singleton uses
-     */
-    internal var databaseHandler: DatabaseHandler = MemoryDatabase()
-
-    /**
-     * Puts data at a namespace + id
-     *
-     * @param model The model object to use.
-     * @param item The item to use
-     */
-    fun <T> put(model: Model<T>, item: T): Boolean {
-
-        val data = model.asData(item)
-
-        return databaseHandler.put(model.dataNamespace, model.dependsOn?.dataNamespace ?: Model.defaultNamespace, id = data.first, data = data.second)
-    }
-
-    fun <T> erase(model: Model<T>, id: ID): Boolean {
-
-        return databaseHandler.erase(model.dataNamespace, model.dependsOn?.dataNamespace ?: Model.defaultNamespace, id)
-
-    }
+    override var databaseHandler: DatabaseHandler = MemoryDatabase()
 
 }
