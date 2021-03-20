687fafec2144dbe5915b859d65f652fdd8be81f5
LeoDog896 <LeoDog896@hotmail.com>
2021-03-20 12:07:15 -0400

    Add Kepi as Extension

---
+++ src/main/kotlin/world/cepi/kepi/Kepi.kt
@@ -0,0 +1,13 @@
+package world.cepi.kepi
+
+import net.minestom.server.extensions.Extension
+
+class Kepi : Extension() {
+    override fun initialize() {
+        logger.info("[Kepi] has been enabled!")
+    }
+
+    override fun terminate() {
+        logger.info("[Kepi] has been disabled!")
+    }
+}
