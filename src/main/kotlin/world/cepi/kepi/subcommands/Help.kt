80f93ba142363239369f7e573718954f28444869
LeoDog896 <LeoDog896@hotmail.com>
2021-03-20 20:06:18 -0400

    Don't use chatcolors in help subcommand

--- src/main/kotlin/world/cepi/kepi/subcommands/Help.kt
+++ src/main/kotlin/world/cepi/kepi/subcommands/Help.kt
@@ -2,7 +2,6 @@ package world.cepi.kepi.subcommands
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import net.minestom.server.chat.ChatColor.*
 import net.minestom.server.command.builder.Command
 
 class Help(vararg val messages: Component, name: String = "help") : Command(name) {
@@ -21,9 +20,8 @@ class Help(vararg val messages: Component, name: String = "help") : Command(name
         setDefaultExecutor { sender, _ ->
             sender.sendMessage(Component.text("$head${arm.repeat(armLength)}", NamedTextColor.DARK_GRAY))
 
-            sender.sendMessage(Component.text("$body $GRAY", NamedTextColor.DARK_GRAY).also {
-                it.append(messages.reduce { acc, component -> acc.append(component) })
-            })
+            sender.sendMessage(Component.text("$body ", NamedTextColor.DARK_GRAY)
+                .append(messages.reduce { acc, component -> acc.append(component).color(NamedTextColor.GRAY) }))
 
             sender.sendMessage(Component.text("$tail${arm.repeat(armLength)}", NamedTextColor.DARK_GRAY))
         }
