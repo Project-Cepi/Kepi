32e01e199fbf71f56323d01e68493c6f3013cc18
LeoDog896 <LeoDog896@hotmail.com>
2021-03-20 20:18:24 -0400

    Use different simpler help message

--- src/main/kotlin/world/cepi/kepi/subcommands/Help.kt
+++ src/main/kotlin/world/cepi/kepi/subcommands/Help.kt
@@ -7,28 +7,19 @@ import net.minestom.server.command.builder.Command
 class Help(vararg val messages: Component, name: String = "help") : Command(name) {
 
     companion object {
-        const val head = "╓"
-        const val tail = "╙"
-
         const val arm = "─"
-        const val armLength = 4
-
-        const val body = "║"
+        const val armLength = 5
     }
 
     init {
         setDefaultExecutor { sender, _ ->
-            sender.sendMessage(Component.text("$head${arm.repeat(armLength)}", NamedTextColor.DARK_GRAY))
+            sender.sendMessage(Component.text(arm.repeat(armLength), NamedTextColor.DARK_GRAY))
 
             messages.forEach {
-                sender.sendMessage(
-                    Component.text(body, NamedTextColor.DARK_GRAY)
-                        .append(Component.space())
-                        .append(it.color(NamedTextColor.GRAY))
-                )
+                sender.sendMessage(it.color(NamedTextColor.GRAY))
             }
 
-            sender.sendMessage(Component.text("$tail${arm.repeat(armLength)}", NamedTextColor.DARK_GRAY))
+            sender.sendMessage(Component.text(arm.repeat(armLength), NamedTextColor.DARK_GRAY))
         }
     }
 
