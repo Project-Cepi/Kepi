b2a34d5dfd748cc1b33ca7a7be34556db0ff1096
LeoDog896 <LeoDog896@hotmail.com>
2021-03-30 19:24:17 -0400

    Use nio

--- src/main/kotlin/world/cepi/kepi/Kepi.kt
+++ src/main/kotlin/world/cepi/kepi/Kepi.kt
@@ -2,8 +2,10 @@ package world.cepi.kepi
 
 import net.minestom.server.extensions.Extension
 import world.cepi.kepi.translations.TranslationRegistry
+import kotlin.io.path.ExperimentalPathApi
 
 class Kepi : Extension() {
+    @ExperimentalPathApi
     override fun initialize() {
 
         TranslationRegistry.grab()
--- src/main/kotlin/world/cepi/kepi/translations/TranslationRegistry.kt
+++ src/main/kotlin/world/cepi/kepi/translations/TranslationRegistry.kt
@@ -3,27 +3,35 @@ package world.cepi.kepi.translations
 import java.io.BufferedInputStream
 import java.io.File
 import java.net.URL
-import java.util.zip.ZipFile
+import java.nio.file.LinkOption
+import java.nio.file.Paths
 import java.util.zip.ZipInputStream
+import kotlin.io.path.*
 
 object TranslationRegistry {
 
+    /** URL to grab the zip from */
     const val url = "https://github.com/Project-Cepi/Translations/releases/download/latest/pack.zip"
-    val translationsFolder = File("./translations")
+    val translationsFolder = Paths.get("./translations")
 
     /**
      * Initializes and unpacks the translations necessary
      */
+    @ExperimentalPathApi
     internal fun grab() {
 
-        if (!translationsFolder.exists()) translationsFolder.mkdirs()
+        if (!translationsFolder.exists()) translationsFolder.createDirectories()
 
         ZipInputStream(BufferedInputStream(URL(url).openStream(), 1024)).use { zipInputStream ->
             generateSequence { zipInputStream.nextEntry }
                 .filterNot { it.isDirectory }
                 .map {
-                    val file = File(translationsFolder, it.name)
-                    file.mkdirs()
+                    val file = translationsFolder.resolve(it.name)
+
+                    file.parent.createDirectories() // create above folder
+                    if (!file.exists()) file.createFile() // create file
+
+                    // write to file
                     file.outputStream().use { fileOutputStream ->
                         zipInputStream.copyTo(fileOutputStream)
                     }
