2045d0a53d395abdd62c5a434b0a42df4e96cf6d
LeoDog896 <LeoDog896@hotmail.com>
2021-06-08 20:51:38 -0400

    Better translation support for non-format

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,12 @@ plugins {
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    jcenter()
+    // Use mavenCentral
     mavenCentral()
 
-    // Use mavenCentral
-    maven(url = "https://repo1.maven.org/maven2/")
-    maven(url = "https://repo.spongepowered.org/maven")
-    maven(url = "https://libraries.minecraft.net")
     maven(url = "https://jitpack.io")
-    maven(url = "https://jcenter.bintray.com/")
+    maven(url = "https://repo.spongepowered.org/maven")
+    maven(url = "https://repo.minestom.com/repository/maven-public/")
     maven(url = "https://repo.velocitypowered.com/snapshots/")
 }
 
--- src/main/kotlin/world/cepi/kepi/messages/translations/TranslationExtensions.kt
+++ src/main/kotlin/world/cepi/kepi/messages/translations/TranslationExtensions.kt
@@ -5,9 +5,16 @@ import net.kyori.adventure.text.TextReplacementConfig
 import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.command.CommandSender
 import net.minestom.server.entity.Player
+import world.cepi.kepi.messages.formatPercent
 import java.lang.Exception
 import java.util.*
 
-fun CommandSender.formatTranslableMessage(namespace: String = "common", key: String): Component {
-    return Component.text(TranslationRegistry[namespace, key, (this as? Player)?.settings?.locale ?: "en_US"] ?: "$namespace.$key")
-}
+fun CommandSender.formatTranslableMessage(
+    namespace: String = "common",
+    key: String,
+    vararg params: Component = arrayOf()
+): Component =
+    Component.text(
+        TranslationRegistry[namespace, key, (this as? Player)?.settings?.locale ?: "en_US"]
+            ?: "$namespace.$key"
+    ).formatPercent(*params)
