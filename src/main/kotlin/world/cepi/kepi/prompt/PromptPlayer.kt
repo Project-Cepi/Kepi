8ee2973e9bde65eeddbfc86ec3ff586ce8146b01
CmdrNorthpaw <thetuxkid@gmail.com>
2021-06-19 21:09:06 +0100

    Delays and error handling

--- src/main/kotlin/world/cepi/kepi/prompt/PromptPlayer.kt
+++ src/main/kotlin/world/cepi/kepi/prompt/PromptPlayer.kt
@@ -2,12 +2,15 @@ package world.cepi.kepi.prompt
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.event.ClickEvent
 import net.minestom.server.command.CommandSender
 import net.minestom.server.entity.Player
+import net.minestom.server.event.player.PlayerDisconnectEvent
+import java.util.concurrent.TimeoutException
 
-suspend fun Player.prompt(prompt: IncompletePrompt, timeout: Long = -1): CompletePrompt {
+suspend fun Player.prompt(prompt: IncompletePrompt, timeout: Long = -1, throwCancelled: Boolean = false): CompletePrompt? {
     val promptText = prompt.options.map {
         it.text.clickEvent(ClickEvent.runCommand("acceptPrompt ${it.id}"))
     }
@@ -21,8 +24,20 @@ suspend fun Player.prompt(prompt: IncompletePrompt, timeout: Long = -1): Complet
     activePrompts[prompt] = rendezvousChannel
 
     this.sendMessage(finalPrompt)
+    if (timeout != -1L) {
+        coroutineScope { launch {
+            delay(timeout)
+            rendezvousChannel.cancel(CancellationException("Prompt timed out", TimeoutException()))
+        } }
+    }
 
-    return rendezvousChannel.receive()
+    this.addEventCallback(PlayerDisconnectEvent::class.java) { rendezvousChannel.cancel() }
+
+    return try {
+        rendezvousChannel.receive()
+    } catch (error: ClosedReceiveChannelException) {
+        if (!throwCancelled) return null else throw error
+    }
 }
 
 fun Player.promptBlocking(prompt: IncompletePrompt, timeout: Long) = runBlocking { prompt(prompt, timeout) }
