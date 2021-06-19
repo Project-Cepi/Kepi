216fbff2bfb3da9b21ac66e6e3bb89d15abe0abb
CmdrNorthpaw <thetuxkid@gmail.com>
2021-06-19 20:16:18 +0100

    New prompt command and some automagic to create confirmation syntax

--- src/main/kotlin/world/cepi/kepi/prompt/Prompt.kt
+++ src/main/kotlin/world/cepi/kepi/prompt/Prompt.kt
@@ -1,16 +1,40 @@
 package world.cepi.kepi.prompt
 
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.channels.trySendBlocking
 import net.kyori.adventure.text.TextComponent
+import net.minestom.server.command.builder.arguments.ArgumentType
+import world.cepi.kstom.command.addSyntax
 import java.util.*
 
 class Prompt(
-    vararg val options: PromptOption
+    val playerId: UUID,
+    vararg val options: PromptOption,
 )
+
+private val activePrompts: MutableMap<Prompt, Channel<Prompt>> = mutableMapOf()
 
 class PromptOption(
     val text: TextComponent,
-    val id: UUID,
     val value: String
 ) {
     var isChosen = false
+    val id: UUID = UUID.randomUUID()
+
+    init {
+        PromptCommand.addSyntax(ArgumentType.Word("choice").from(id.toString())) { sender, args ->
+            val choiceId = UUID.fromString(args["choice"])
+            val prompt = activePrompts.keys.firstOrNull { it.options.any { it.id == choiceId } } ?: return@addSyntax
+
+            val newPrompt = Prompt(prompt.playerId,
+                    *prompt.options.map {
+                if (it.id == choiceId) { it.isChosen = true }
+                it
+            }.toTypedArray())
+
+            activePrompts[prompt]?.trySendBlocking(newPrompt)
+
+        }
+    }
 }
