dd1a885fc84382207f4c7897842f055f10dc74c6
LeoDog896 <LeoDog896@hotmail.com>
2021-09-08 10:07:18 -0400

    Clean up message format

--- src/main/kotlin/world/cepi/kepi/command/BlockHandlerCommand.kt
+++ src/main/kotlin/world/cepi/kepi/command/BlockHandlerCommand.kt
@@ -1,5 +1,8 @@
 package world.cepi.kepi.command
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import net.minestom.server.command.builder.Command
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
@@ -23,7 +26,10 @@ object BlockHandlerCommand : Command("blockhandler") {
             }
 
             player.sendMessage(
-                "${handler.namespaceId}: ${handler::class.simpleName ?: "Unknown Name"}"
+                Component.text(handler.namespaceId.toString(), NamedTextColor.GRAY)
+                    .append(Component.text(" // ", NamedTextColor.DARK_GRAY))
+                    .append(Component.text(handler::class.simpleName ?: "Unknown Name", NamedTextColor.BLUE))
+                    .append(Component.text(" (" + (handler::class.qualifiedName ?: "Unknown Qualified Name") + ")", TextColor.color(145, 145, 145)))
             )
         }
     }
