64d9cbcaef1549c421037d4588a43041cd5c59be
LeoDog896 <LeoDog896@hotmail.com>
2021-09-12 08:15:21 -0400

    Provide data on block handler

--- src/main/kotlin/world/cepi/kepi/command/BlockHandlerCommand.kt
+++ src/main/kotlin/world/cepi/kepi/command/BlockHandlerCommand.kt
@@ -1,6 +1,7 @@
 package world.cepi.kepi.command
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 import net.minestom.server.command.builder.Command
@@ -16,9 +17,10 @@ object BlockHandlerCommand : Command("blockhandler") {
         addSyntax(relativeBlockPosition) {
             val player = sender as? Player ?: return@addSyntax
 
-            val handler = player.instance!!
+            val block = player.instance!!
                 .getBlock(context[relativeBlockPosition].from(player))
-                .handler()
+
+            val handler = block.handler()
 
             if (handler == null) {
                 player.sendMessage("No block handler found!")
@@ -30,6 +32,20 @@ object BlockHandlerCommand : Command("blockhandler") {
                     .append(Component.text(" // ", NamedTextColor.DARK_GRAY))
                     .append(Component.text(handler::class.simpleName ?: "Unknown Name", NamedTextColor.BLUE))
                     .append(Component.text(" (" + (handler::class.qualifiedName ?: "Unknown Qualified Name") + ")", TextColor.color(145, 145, 145)))
+                    .append(Component.newline())
+                    .append(Component.newline())
+                    .let textComponent@ {
+                        block.nbt()?.toSNBT()?.let { snbt ->
+                            return@textComponent it.append(
+                                Component.text(snbt, NamedTextColor.GRAY)
+                                    .hoverEvent(Component.text("Click to copy", NamedTextColor.YELLOW))
+                                    .clickEvent(ClickEvent.copyToClipboard(snbt))
+                            )
+                        }
+
+                        it
+                    }
+
             )
         }
     }
