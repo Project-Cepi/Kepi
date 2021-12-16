10251d7be3da7668f3d0e3958acd2f4b012bc2d6
LeoDog896 <LeoDog896@hotmail.com>
2021-12-15 19:24:18 -0500

    Safer creational result create

--- src/main/kotlin/world/cepi/kepi/item/AddCreationalItem.kt
+++ src/main/kotlin/world/cepi/kepi/item/AddCreationalItem.kt
@@ -2,13 +2,23 @@ package world.cepi.kepi.item
 
 import net.minestom.server.entity.Player
 import net.minestom.server.item.ItemStack
+import world.cepi.gooey.InventoryManager
 
 object AddCreationalItem {
 
     fun put(player: Player, itemStack: ItemStack): CreationalItemResult {
-
         if (player.inventory.itemStacks.none { it.isAir }) {
             return CreationalItemResult.CouldNotPut
+        }
+
+        if (InventoryManager.slot(player.heldSlot.toInt()) != null) {
+            // The player is holding a non-movable item. Move the created item to a new slot.
+            val index = player.inventory.itemStacks.indexOfFirst { it.isAir }
+            player.inventory.setItemStack(index, itemStack)
+            if (index in 0..8) {
+                player.setHeldItemSlot(index.toByte())
+            }
+            return CreationalItemResult.InInventoryItemResult(index)
         }
 
         if (player.itemInMainHand == ItemStack.AIR) {
