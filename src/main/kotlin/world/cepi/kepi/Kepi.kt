bcccd1e9b004946516a4526a6bafc38884e5ddae
Tristan <LeoDog896@hotmail.com>
2022-02-07 16:04:26 -0500

    Cleanup

--- src/main/kotlin/world/cepi/kepi/Kepi.kt
+++ src/main/kotlin/world/cepi/kepi/Kepi.kt
@@ -4,9 +4,13 @@ import com.mattworzala.canvas.ext.canvas
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import net.minestom.server.event.EventNode.event
+import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.event.player.PlayerUseItemEvent
 import net.minestom.server.extensions.Extension
 import net.minestom.server.item.Material
+import net.minestom.server.scoreboard.Sidebar
+import net.minestom.server.scoreboard.Sidebar.ScoreboardLine
 import world.cepi.gooey.InventoryManager
 import world.cepi.kepi.command.*
 import world.cepi.kepi.menu.menuUI
@@ -19,6 +23,7 @@ import world.cepi.kstom.event.listenOnly
 import world.cepi.kstom.item.item
 import world.cepi.kstom.util.log
 import world.cepi.kstom.util.node
+
 
 class Kepi : Extension() {
 
